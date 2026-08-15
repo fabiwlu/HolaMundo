546b360c621eaaf35798e3c761f9fd9c7b3fad42
Fabiwlu <fabiluu67@gmail.com>
2026-08-15 19:26:48 -0300

    Initial commit

---
+++ src/RegistroEstudiantes.java
@@ -0,0 +1,21 @@
+
+package registroestudiantes;
+
+public class RegistroEstudiantes {
+
+    public static void main(String[] args){
+
+        //Asignación al Registro con '='
+
+        String nombre = "Fabiola", carrera = "Ingenieria Informática";
+        int edad = 20, semestre = 4;
+        double promedio = 3.00;
+        boolean esMayor = true;
+
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Estudio la Carrera de: " + carrera);
+        System.out.println("Tengo: "+edad+"\ny estoy en el "+semestre+"\nsemestre");
+        System.out.println("Tengo de promedio: "+ promedio);
+        System.out.println("Soy mayor: " + esMayor);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+package holamundo;
+
+public class Variables {
+        public static void main(String[] args) {
+
+            //Operación de Asignación '='
+
+            int edad = 32;
+            double estatura = 1.65;
+            boolean tieneCarnet = false;
+            char letra = 'L';
+            String direccion = "Suscribite 123";
+            long numeroLargo = 54223232 ;
+
+            System.out.println("Hola! Mi edad es: " + edad );
+            System.out.println("Mi estatura es: " + estatura);
+            System.out.println("Dirección: " + direccion);
+            System.out.println("Tiene carnet de conducir: "+tieneCarnet);
+    }
+}
